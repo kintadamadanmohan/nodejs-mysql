4c4c1daf41f284d8fede32e4fbe83c11ff01335d
kintadamadanmohan <99582786+kintadamadanmohan@users.noreply.github.com>
2022-02-19 18:52:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const routes = require('./src/routes/routes');
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(9898, () => {
+    console.log('Server running!');
+});
