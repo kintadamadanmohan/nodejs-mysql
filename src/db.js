003a0147071424368628271c30af10f05f47855a
kintadamadanmohan <99582786+kintadamadanmohan@users.noreply.github.com>
2022-02-19 20:54:08 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,14 @@ const mysql = require('mysql');
 
 require('dotenv').config();
 
+console.log({
+    host: 'localhost' | process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
 const database = mysql.createConnection({
+    host: 'localhost' | process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
